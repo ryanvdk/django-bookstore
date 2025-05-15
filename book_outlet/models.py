292f22a0aa9abc5b8e1a863f4cb21be0d316e2b1
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-15 14:56:04 -0700

    Created Book model and migration

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxLengthValidator
 
 # Create your models here.
 
@@ -6,4 +7,11 @@ from django.db import models
 class Book(models.Model):
     # char field type max_length is required, ID column is automatically created.
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxLengthValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+
+    # Overrides how the object is represented when the string method is called, so for example how it is displayed in the console.
+    def __str__(self):
+        return f"{self.title}{" by " + self.author if self.author else ""} ({self.rating}) {"Best Seller" if self.is_bestselling else ""}"
