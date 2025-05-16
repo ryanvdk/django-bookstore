7e18ccadb4dd1ed031d1658d7bed5ba656c4cc72
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-16 15:31:10 -0700

    Added Author class and connected it to the Book model.

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -6,20 +6,26 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     # char field type max_length is required, ID column is automatically created.
     title = models.CharField(max_length=50)
     # rating = models.IntegerField(
     #          validators=[MinValueValidator(1), MaxLengthValidator(5)])
     rating = models.IntegerField()
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="",
                             null=False, db_index=True)
 
     # Overrides how the object is represented when the string method is called, so for example how it is displayed in the console.
     def __str__(self):
-        return f"{self.title}{" by " + self.author if self.author else ""} ({self.rating}) {"Best Seller" if self.is_bestselling else ""}"
+        return f"{self.title}"
 
     def get_absolute_url(self):
         return reverse("book_details", args=[self.slug])
